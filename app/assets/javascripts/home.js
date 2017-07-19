59626547b320d3cc4fe5a98dc95be4a5bac365b7
huntojkojqw01 <hungnd.k58@gmail.com>
2017-07-19 23:22:27 +0700

    epi8:nested component

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,22 @@
+Vue.component('task-list',{
+	template: `
+		<div>
+			<task v-for="task in tasks">{{task.description}}</task>
+		</div>
+	`,
+	data: function(){
+		return {
+			tasks: [
+				{description: 'Task Mot',completed: true},
+				{description: 'Task Hai',completed: false},
+				{description: 'Task Ba',completed: true},
+				{description: 'Task Bon',completed: true},
+				{description: 'Task Nam',completed: false},
+				{description: 'Task Sau',completed: true}
+			]
+		}
+	}		
+});
 Vue.component('task',{
-		template: '<li>Hung <slot></slot></li>'
-	});
+	template: '<li><slot></slot></li>'
+});
