d848e8bd51ac897e2680e80641ebdf463e634b56
huntojkojqw01 <hungnd.k58@gmail.com>
2017-07-20 12:10:25 +0700

    epi13:com comunicate:use dispatcher

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,51 @@
+window.Event=new class {
+	constructor(){
+		this.vue=new Vue();
+	}
+	kick(event,data=null){
+		this.vue.$emit(event,data);
+	}
+	listen(event,callback){
+		this.vue.$on(event,callback);
+	}
+}
 Vue.component('cou',{
-	template: '<div><input @blur="applied"></div>',
-	methods:{
-		applied(){
-			this.$emit('chem');
+	template: '<div>Cou<input @change="send" v-model="content"><p>{{receive}}</p></div>',
+	data(){
+		return {
+			receive: "",
+			content: ""
 		}
+	},
+	methods:{
+		send(){
+			Event.kick('cou-send',this.content);
+		},
+		recv(mess){
+			this.receive=mess;
+		}
+	},
+	created(){
+		Event.listen('acp-send',this.recv);
+	}
+});
+Vue.component('acp',{
+	template: '<div>Acp<input @change="send" v-model="content"><p>{{receive}}</p></div>',
+	data(){
+		return {
+			receive: "",
+			content: ""
+		}
+	},
+	methods:{
+		send(){
+			Event.kick('acp-send',this.content);
+		},
+		recv(mess){
+			this.receive=mess;
+		}
+	},
+	created(){
+		Event.listen('cou-send',this.recv);
 	}
 });
