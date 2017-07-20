f190ef9ad7e74bb959d9cdc1c4c6215aecc080cc
huntojkojqw01 <hungnd.k58@gmail.com>
2017-07-20 09:16:48 +0700

    epi10:com modal

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,46 +1,59 @@
-Vue.component('task-list',{
+// Vue.component('task-list',{
+// 	template: `
+// 		<div>
+// 			<task v-for="task in tasks">{{task.description}}</task>
+// 		</div>
+// 	`,
+// 	data: function(){
+// 		return {
+// 			tasks: [
+// 				{description: 'Task Mot',completed: true},
+// 				{description: 'Task Hai',completed: false},
+// 				{description: 'Task Ba',completed: true},
+// 				{description: 'Task Bon',completed: true},
+// 				{description: 'Task Nam',completed: false},
+// 				{description: 'Task Sau',completed: true}
+// 			]
+// 		}
+// 	}		
+// });
+// Vue.component('task',{
+// 	template: '<li><slot></slot></li>'
+// });
+// Vue.component('message',{
+// 	props: ['title','body'],
+// 	data(){
+// 		return {
+// 			isVisible: true
+// 		}
+// 	},
+// 	template: `
+// 		<article class="message" v-show="isVisible">
+// 		  <div class="message-header">
+// 		    <p>{{title}}</p>
+// 		    <button class="delete" @click="hideModal"></button>
+// 		  </div>
+// 		  <div class="message-body">
+// 				{{body}}
+// 		  </div>
+// 		</article>
+// 	`,
+// 	methods: {
+// 		hideModal(){
+// 			this.isVisible=false;
+// 		}
+// 	}
+// });
+Vue.component('modal',{	
 	template: `
-		<div>
-			<task v-for="task in tasks">{{task.description}}</task>
+		<div class="modal is-active">
+		  <div class="modal-background"></div>
+		  <div class="modal-content">
+		  	<div class="box">
+		  		Any other Bulma elements you want
+		  	</div>
+		  </div>
+		  <button class="modal-close is-large" @click="$emit('close')"></button>
 		</div>
-	`,
-	data: function(){
-		return {
-			tasks: [
-				{description: 'Task Mot',completed: true},
-				{description: 'Task Hai',completed: false},
-				{description: 'Task Ba',completed: true},
-				{description: 'Task Bon',completed: true},
-				{description: 'Task Nam',completed: false},
-				{description: 'Task Sau',completed: true}
-			]
-		}
-	}		
-});
-Vue.component('task',{
-	template: '<li><slot></slot></li>'
-});
-Vue.component('message',{
-	props: ['title','body'],
-	data(){
-		return {
-			isVisible: true
-		}
-	},
-	template: `
-		<article class="message" v-show="isVisible">
-		  <div class="message-header">
-		    <p>{{title}}</p>
-		    <button class="delete" @click="hideModal"></button>
-		  </div>
-		  <div class="message-body">
-				{{body}}
-		  </div>
-		</article>
-	`,
-	methods: {
-		hideModal(){
-			this.isVisible=false;
-		}
-	}
+	`
 });
