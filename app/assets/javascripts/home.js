b74dd61d73ccf68b1aa73034d66b381b9fe27de8
huntojkojqw01 <hungnd.k58@gmail.com>
2017-07-20 10:51:15 +0700

    epi12:com comunicate:custom event

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,44 +1,8 @@
-Vue.component('tabs',{
-	data(){
-		return {
-			tabs: []
+Vue.component('cou',{
+	template: '<div><input @blur="applied"></div>',
+	methods:{
+		applied(){
+			this.$emit('chem');
 		}
-	},
-	template: `
-		<div>
-			<div class="tabs">
-			  <ul>
-			    <li v-for="tab in tabs" :class="{'is-active': tab.isActive}"><a @click="tabselect(tab)">{{tab.name}}</a></li>			    
-			  </ul>
-			</div>
-			<div><slot></slot></div>
-		</div>	
-	`,
-	created(){
-		this.tabs=this.$children;//lấy dữ liệu từ chính con nó trogn slot
-	},
-	methods: {
-		tabselect(selectedTab){
-			this.tabs.forEach(tab=>{
-				tab.isActive=(tab.name==selectedTab.name);
-			});
-		}
-	}
-});
-Vue.component('tab',{
-	template: `
-		<div v-show="isActive"><slot></slot></div>
-	`,
-	props: {
-		name: {require: true},
-		selected: {default: false}
-	},
-	data(){
-		return {
-			isActive: false
-		}
-	},
-	mounted(){
-		this.isActive=this.selected;
 	}
 });
