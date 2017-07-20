ec136787e59a33cb6d9cdc3938e13bae09b231bb
huntojkojqw01 <hungnd.k58@gmail.com>
2017-07-20 08:31:49 +0700

    epi9:com message

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -20,3 +20,27 @@ Vue.component('task-list',{
 Vue.component('task',{
 	template: '<li><slot></slot></li>'
 });
+Vue.component('message',{
+	props: ['title','body'],
+	data(){
+		return {
+			isVisible: true
+		}
+	},
+	template: `
+		<article class="message" v-show="isVisible">
+		  <div class="message-header">
+		    <p>{{title}}</p>
+		    <button class="delete" @click="hideModal"></button>
+		  </div>
+		  <div class="message-body">
+				{{body}}
+		  </div>
+		</article>
+	`,
+	methods: {
+		hideModal(){
+			this.isVisible=false;
+		}
+	}
+});
