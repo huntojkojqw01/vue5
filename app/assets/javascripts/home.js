c349cb9d3ece1c20cd4f85a3af0d867f3bf9845a
huntojkojqw01 <hungnd.k58@gmail.com>
2017-07-20 16:12:59 +0700

    epi15:component and inline temp

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,25 +1,7 @@
-Vue.component('modal',{
-	template: `
-		<div class="modal is-active">
-		  <div class="modal-background"></div>
-		  <div class="modal-card">
-		    <header class="modal-card-head">
-		      <p class="modal-card-title">
-		      	<slot name="header"></slot>
-		      </p>
-		      <button class="delete"></button>
-		    </header>
-		    <section class="modal-card-body">
-		      <slot name="body"></slot>
-		      <slot>
-		      	default content here
-		      </slot>
-		    </section>
-		    <footer class="modal-card-foot">
-		      <a class="button is-success">Save changes</a>
-		      <a class="button">Cancel</a>
-		    </footer>
-		  </div>
-		</div>
-	`
+Vue.component('hero',{
+	data(){
+		return {
+			message: 10
+		}
+	}
 });
